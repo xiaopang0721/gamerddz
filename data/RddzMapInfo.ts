d2a05616257b613369d552b039b9c7e74e11d97d
happy <happy>
2019-10-10 22:09:08 +0800

    斗地主牌局详情

--- data/RddzMapInfo.ts
+++ data/RddzMapInfo.ts
@@ -35,32 +35,75 @@ module gamerddz.data {
 			}
 		}
 
-		public getBattleInfoToObj(): string {
-			let str: string = "";
+		private _roundCount: number;//回合计数，方便房卡不同局分割开
+		private _showcardCount: number = 0;//摊牌计数，方便重置标识
+		private _addRound: boolean = false;//回合标题已增加
+		private _addDiZhu: boolean = false;//抢地主标题已增加
+		private _addFirst: boolean = false;//先手标题已增加
+		private _addShowCards: boolean = false;//结束摊牌标题已增加
+		private _addSettle: boolean = false;//结算标题已增加
+		public getBattleInfoToObj(): any {
+			let battleObj: any[] = [];
+			this._roundCount = 1;
 			for (let i = 0; i < this._battleInfoMgr.info.length; i++) {
-				let battleInfo = this._battleInfoMgr.info[i] as gamecomponent.object.BattleInfoBase;
-				let name = this.GetPlayerNameFromSeat(battleInfo.SeatIndex)
-				if (battleInfo.Type == 35) {	//定下地主
-					let info = this._battleInfoMgr.info[i] as gamecomponent.object.BattleInfoPass;
-					let newString = "地主是：" + name;
-					if (!this.isFirst) {
-						this.isFirst = true;
-						if (str == "") {
-							str = newString;
-						} else {
-							str = str + "#" + "" + "#" + newString;
-						}
-					} else {
-						str = str + "#" + newString;
+				let info = this._battleInfoMgr.info[i] as gamecomponent.object.BattleInfoBase;
+				let name = this.GetPlayerNameFromSeat(info.SeatIndex) + "：";
+				if (!this._addRound) {//局数信息
+					this._addRound = true;
+					battleObj.push({ type: 1, title: StringU.substitute("第{0}局", this._roundCount) });
+				}
+				if (info instanceof gamecomponent.object.BattleInfoJiaoDiZhu) {	//抢地主
+					if (!this._addDiZhu) {
+						this._addDiZhu = true;
+						battleObj.push({ type: 2, title: "开始抢地主" });
 					}
-				} else if (battleInfo.Type == 11) {	//结算
-					let info = this._battleInfoMgr.info[i] as gamecomponent.object.BattleInfoSettle;
-					let newString = name + "盈利：" + info.SettleVal;
-					str = str + "#" + newString;
-					this.isFirst = false;
+					let desc = info.OptType == 1 ? name + "抢地主" : name + "不抢地主";
+					battleObj.push({ type: 6, desc: desc });
+				} else if (info instanceof gamecomponent.object.BattleInfoQiangDZEnd) { //定地主
+					let desc = name + HtmlFormat.addHtmlColor("抢得地主", TeaStyle.COLOR_GREEN);
+					battleObj.push({ type: 6, desc: desc });
+				} else if (info instanceof gamecomponent.object.BattleInfoSeeCard) { //先手
+					if (!this._addFirst) {
+						this._addFirst = true;
+						battleObj.push({ type: 2, title: "先手玩家" });
+					}
+					let desc = name + HtmlFormat.addHtmlColor("地主", TeaStyle.COLOR_GREEN);
+					battleObj.push({ type: 6, desc: desc });
+				} else if (info instanceof gamecomponent.object.BattleInfoSettle) {	//结算
+					if (!this._addSettle) {
+						this._addSettle = true;
+						battleObj.push({ type: 2, title: "开始结算" });
+					}
+					let desc: string = "";
+					if (info.SettleVal > 0) {
+						desc = name + " 积分 " + HtmlFormat.addHtmlColor("+" + info.SettleVal.toString(), TeaStyle.COLOR_GREEN)
+					} else if (info.SettleVal < 0) {
+						desc = name + " 积分 " + HtmlFormat.addHtmlColor(info.SettleVal.toString(), TeaStyle.COLOR_RED)
+					} else {
+						desc = name + " 积分 +" + info.SettleVal;
+					}
+					battleObj.push({ type: 6, desc: desc });
+				} else if (info instanceof gamecomponent.object.BattleInfoShowCards) { //摊牌
+					if (!this._addShowCards) {
+						this._addShowCards = true;
+						battleObj.push({ type: 2, title: "结束余牌" });
+					}
+					let desc: string = "[" + info.CardType + "]";
+					let cards = info.Cards;
+					battleObj.push({ type: 3, name: name, desc: desc, cards: cards });
+					this._showcardCount++;
+					if (this._showcardCount == this.GetPlayerNumFromSeat()) {
+						this._roundCount++;//下一回合计数
+						this._addRound = false;
+						this._addDiZhu = false;
+						this._addFirst = false;
+						this._addShowCards = false;
+						this._addSettle = false;
+						this._showcardCount = 0;
+					}
 				}
 			}
-			return str;
+			return battleObj;
 		}
 
 		//通过座位取玩家名字
@@ -69,6 +112,16 @@ module gamerddz.data {
 			let users = this._battleInfoMgr.users;
 			name = users[index - 1].name;
 			return name
+		}
+	
+		//遍历座位获取玩家总数
+		private GetPlayerNumFromSeat(): number {
+			let num: number = 0;
+			for (let i = 0; i < this._battleInfoMgr.users.length; i++) {
+				let user = this._battleInfoMgr.users[i];
+				if (user.name) num++;
+			}
+			return num
 		}
 	}
 }
